7325de5781820844a5d1be5e6bf60e4d631a4a36
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-04-11 15:45:51 -0700

    commit tree 17025d044896559cf2a68d8d65260d9d61b27b0c

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+    end
+    
+    get '/name' do
+        "My name is Laika."
+    end
+    
+    get '/hometown' do
+        "My hometown is Oakland. "
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is WAP."
+    end
 end
+
+
